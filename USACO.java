aee5b131652cea04bf57d984352404a523a08b4e
jwu03 <jwu03@stuy.edu>
2019-03-10 22:39:06 -0400

    adding helper function

--- USACO.java
+++ USACO.java
@@ -1,13 +1,14 @@
+import java.util.*;
+import java.io.*;
 public class USACO{
-  public static int bronze(String filename){
+  public static int bronze(String filename)throws FileNotFoundException{
     int R , C , E , N ;
     File text = new File(filename);
     Scanner s = new Scanner(text);
-    try{
     R = s.nextInt();
     C = s.nextInt();
     E = s.nextInt();
-    N = s.NextInt();
+    N = s.nextInt();
     int[][] field = new int[R][C];
     for(int i = 0; i < R;i++){
       for(int j = 0; j < C;j++){
@@ -20,9 +21,26 @@ public class USACO{
         digging[i][j] = s.nextInt();
       }
     }
-  }catch(Exceptions e){
-    e.printStackTrace();
+    for(int i = 0; i< N;i++){
+      bronzeH(field,digging[i][0] - 1,digging[i][1] - 1,digging[i][2]);
+    }
   }
-
+  public static int[][] bronzeH(int[][] field, int row, int col, int level) {
+  int biggest = 0;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j ++) {
+      if (field[row + i][col + j] > biggest) {
+        biggest = field[row + i][col + j];
+      }
+    }
   }
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j ++) {
+      if (biggest - field[row + i][col + j] <= level) {
+        field[row + i][col + j] = biggest - level;
+      }
+    }
+  }
+  return field;
+}
 }
